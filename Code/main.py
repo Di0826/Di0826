b85503495784c38b89be807bdee275115725a7e6
yingjia <yk_chai@yeah.net>
2021-06-01 23:38:33 +0800

    svd collaborative filtering (funkSVD) done

--- Code/main.py
+++ Code/main.py
@@ -3,22 +3,23 @@ import data_reader
 import svd_cf
 user_data = data_reader.read_user()
 item_data = data_reader.read_item()
-latent_variable_SVD = 3  # used by svd
+latent_variable_SVD = 8  # used by svd
 learning_rate_SVD = 0.01
+maximum_epoches = 25
+regularization = 0.005
 
 for test_num in range(1, 2):
     print("Test" + str(test_num) + ":")
 
     train = data_reader.read_rating_train(test_num)
+    test = data_reader.read_rating_test(test_num)
 
     train_matrix = np.zeros((user_data.shape[0], item_data.shape[0]))
     # shape = num_user * num_items
     train_matrix[train[:, 0], train[:, 1]] = train[:, 2]
-    predict_matrix = svd_cf.svd_collaborative_filtering(train, train_matrix, latent_variable_SVD, learning_rate_SVD)
+    predict_matrix = svd_cf.svd_collaborative_filtering(train, train_matrix, latent_variable_SVD,
+                                                        learning_rate_SVD, maximum_epoches, regularization)
     # using svd to fill the matrix
-
-    test = data_reader.read_rating_test(test_num)
-
-    print("MSE loss is equal to " + str(svd_cf.RMSE_loss(predict_matrix, test)))
+    print("RMSE loss of " + "test " + str(test_num) + " is equal to " + str(svd_cf.RMSE_loss(predict_matrix, test)))
 
 
--- Code/svd_cf.py
+++ Code/svd_cf.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def svd_collaborative_filtering(train, train_matrix, latent_variable=3,
                                 learning_rate=0.1, maximum_epoches=100, regularization=0.005):
     # below code is an implement of funk-svd, following the https://github.com/gbolmier/funk-svd/README.md
@@ -17,15 +16,17 @@ def svd_collaborative_filtering(train, train_matrix, latent_variable=3,
 
     r_average = np.mean(R[R != 0])
     user_base = np.zeros(num_user)
+    item_base = np.zeros(num_item)
+    '''
     for i in range(num_user):
         temp = R[i, :]
         if np.sum(temp != 0):
             user_base[i] = np.mean(temp[temp != 0]) - r_average
-    item_base = np.zeros(num_item)
     for i in range(num_item):
         temp = R[:, i]
         if np.sum(temp != 0):
             item_base[i] = np.mean(temp[temp != 0]) - r_average
+    '''
     # there are other ways to initialize user_base, item_base and P,Q
 
     last_loss = np.inf
@@ -51,29 +52,26 @@ def svd_collaborative_filtering(train, train_matrix, latent_variable=3,
                 Q[variable_idx, item] += learning_rate * (error * P[user, variable_idx]
                                                           - regularization * Q[variable_idx, item])
         '''
-        the code below are low
         
         '''
-        R_predict = P @ Q + r_average
-        R_predict = R_predict + user_base[:, np.newaxis] + item_base[np.newaxis, :]
-        loss = RMSE_loss(R_predict, train)
+        predict_matrix = P @ Q + r_average
+        predict_matrix = predict_matrix + user_base[:, np.newaxis] + item_base[np.newaxis, :]
+        loss = RMSE_loss(predict_matrix, train)
         if loss < last_loss:
-            print("epoch = " + str(epoch+1))
-            print("MSE loss is equal to " + str(loss))
+            print("At epoch = " + str(epoch+1) + ", RMSE loss on training set is equal to " + str(loss))
             last_loss = loss
         else:
-            print("early stop at epoch" + str(epoch+1) + ", because the loss increased.")
+            print("Early stop at epoch" + str(epoch+1) + ", because the loss increased.")
             break
 
-    R_predict = P @ Q + r_average
-    R_predict = R_predict + user_base[:, np.newaxis] + item_base[np.newaxis, :]
-    return R_predict
+    predict_matrix = P @ Q + r_average
+    predict_matrix = predict_matrix + user_base[:, np.newaxis] + item_base[np.newaxis, :]
+    return predict_matrix
 
 
 def RMSE_loss(predict_matrix, test):
-    error = 0
-    n = test.shape[0]
-    for i in range(n):
-        error += (test[i, 2] - predict_matrix[test[i, 0], test[i, 1]]) ** 2
-    error = np.sqrt(error / n)
-    return error
+    predict = predict_matrix[test[:, 0], test[:, 1]]
+    MSE = np.sum(np.square(predict - test[:, 2]))/test.shape[0]
+    RMSE = np.sqrt(MSE)
+
+    return RMSE
