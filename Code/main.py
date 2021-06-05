49f7873ed730b724b1c8ea7d9e59dd9e728b8873
Zeji Hui <u6987679@anu.edu.au>
2021-06-05 12:27:39 +1000

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -4,8 +4,19 @@ import svd_cf
 import visual
 import neuralnet
 
+
+
+
 def recommendation():
-    user_data = data_reader.read_user()
+    '''
+    
+
+    Returns
+    -------
+    run the moview recommandation system and visualize 100 movie data
+
+    '''
+    user_data = data_reader.read_user() 
     # 943 users, 2 features, gender and age
     item_data = data_reader.read_item()
     # 1682 items(movies), 20 features, the labels (category) and the year of release date
@@ -14,7 +25,7 @@ def recommendation():
     maximum_epoches_SVD = 25  # recommended value 25
     regularization_SVD = 0.005  # recommended value 0.005
 
-    for test_num in range(1, 2):
+    for test_num in range(1, 3):
         print("Test" + str(test_num) + ":")
 
         train = data_reader.read_rating_train(test_num)
@@ -23,13 +34,20 @@ def recommendation():
         # svd
         predict_matrix = svd_cf.svd_collaborative_filtering(train, user_data, item_data, latent_variable_SVD,
                                                             learning_rate_SVD, maximum_epoches_SVD, regularization_SVD)
+        
+        
         print("RMSE loss of SVD CF on test %d is equal to %.6f" %(test_num, svd_cf.RMSE_loss(predict_matrix, test)))
-        # visual.visulize_item(predict_matrix, item_data)
+        print("MAE loss of SVD CF on test %d is equal to %.6f" %(test_num, svd_cf.MAE_loss(predict_matrix, test)))
+        visual.visulize_item(predict_matrix, item_data)
+        
 
         # neural net
-        predict_matrix = neuralnet.neuralnet_predictor(train, user_data, item_data)
-        print("RMSE loss of Neural Net on test %d is equal to %.6f" %(test_num, svd_cf.RMSE_loss(predict_matrix, test)))
-        # visual.visulize_item(predict_matrix, item_data)
+        predict_matrix2 = neuralnet.neuralnet_predictor(train, user_data, item_data)
+        print("RMSE loss of Neural Net on test %d is equal to %.6f" %(test_num, svd_cf.RMSE_loss(predict_matrix2, test)))
+        print("MAE loss of SVD CF on test %d is equal to %.6f" %(test_num, svd_cf.MAE_loss(predict_matrix2, test)))
+
+        
+        #visual.visulize_item(predict_matrix2, item_data)
 
 def discrete(data):
     data = np.where(data < 1.5, 1, data)
